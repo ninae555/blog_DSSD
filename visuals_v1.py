ac8b805498a3b7effd7c04b0a9cfd9f85c394eb4
ninae555 <112419165+ninae555@users.noreply.github.com>
2023-06-15 14:17:25 -0400

    build

--- visuals_v1.py
+++ visuals_v1.py
@@ -4,7 +4,29 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
-energy_saved = pd.read_csv('../input/singapore-waste-management/waste_energy_stat.csv')
-waste_03_17 = pd.read_csv('../input/singapore-waste-management/2003_2017_waste.csv')
-waste_18_20 = pd.read_csv('../input/singapore-waste-management/2018_2020_waste.csv')
+
+energy_saved = pd.read_csv('waste_energy_stat.csv')
+
+waste_03_17 = pd.read_csv('2003_2017_waste.csv')
+waste_18_20 = pd.read_csv('2018_2020_waste.csv')
 # %%
+# Print the first 5 rows of energy_saved
+
+energy_saved.head()
+#%%
+# Print the first 5 rows of waste_03_17
+
+waste_03_17.head()
+#%%
+# Print the first 5 rows of waste_18_20
+
+waste_18_20.head()
+
+# %%
+from functions import null_checker
+
+df_nan = null_checker(energy_saved)
+
+# Print df_nan
+# %%
+# Plot a line chart to show the trend of total energy saved over the years
