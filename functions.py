3689982f3d03e6ddf839e2be198753763a2f0ef9
ninae555 <112419165+ninae555@users.noreply.github.com>
2023-06-15 14:11:35 -0400

    buld: functions

---
+++ functions.py
@@ -0,0 +1,8 @@
+def null_checker(variable):
+    if variable is None:
+        print("Variable is null.")
+    elif isinstance(variable, float) and math.isnan(variable):
+        print("Variable contains NaN.")
+    else:
+        print("Variable is not null and does not contain NaN.")
+
